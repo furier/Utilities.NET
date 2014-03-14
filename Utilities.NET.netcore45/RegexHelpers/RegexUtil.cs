73bf6705a0c191fd5f050a53f260af47a758b993
Sander Struijk <sander.struijk@bouvet.no>
2014-03-14 11:00:20 +0100

    Added window8.1 class library

---
+++ Utilities.NET.netcore45/RegexHelpers/RegexUtil.cs
@@ -0,0 +1,34 @@
+﻿#region File Header
+
+// // ***********************************************************************
+// // Author           : Sander Struijk
+// // ***********************************************************************
+
+#endregion
+
+#region Using statements
+
+using System.Text.RegularExpressions;
+
+#endregion
+
+namespace Utilities.NET.netcore45.RegexHelpers
+{
+    public static class RegexUtil
+    {
+        /// <summary>   Query if 'pattern' is a valid regular expression. </summary>
+        /// <remarks>   Sander.struijk, 13.03.2014. </remarks>
+        /// <param name="pattern">  Specifies the pattern. </param>
+        /// <returns>   true if regular expression pattern valid, false if not. </returns>
+        public static bool IsRegexPatternValid(string pattern)
+        {
+            try
+            {
+                new Regex(pattern);
+                return true;
+            }
+            catch {}
+            return false;
+        }
+    }
+}
