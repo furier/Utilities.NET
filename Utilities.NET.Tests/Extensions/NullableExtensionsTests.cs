8a8875160810e380b24a0d3ed4e3e362b30b02d4
Sander Struijk <sander.struijk@bouvet.no>
2017-09-08 14:30:50 +0200

    Adding nullable extensions

---
+++ Utilities.NET.Tests/Extensions/NullableExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using Utilities.NET.Extensions;
+
+namespace Utilities.NET.Tests.Extensions
+{
+    public class NullableExtensionsTests : UnitTestBase
+    {
+        [Test]
+        [TestCase(null, ExpectedResult = true)]
+        [TestCase(0, ExpectedResult = false)]
+        [TestCase(42, ExpectedResult = false)]
+        public bool HasNoValue(int? value) => value.HasNoValue();
+
+        [Test]
+        public void NullableDateTime_HasNoValue()
+        {
+            DateTime? value = null;
+
+            value.HasNoValue().Should().BeTrue();
+
+            value = DateTime.Now;
+
+            value.HasNoValue().Should().BeFalse();
+        }
+    }
+}
